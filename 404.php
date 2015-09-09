d812bdd3dc53e2e67e6b523977e47e28920852cd
Matt Dunn <matt@processyellow.ca>
2015-09-08 21:47:59 -0400

    Add container to 404 page

--- 404.php
+++ 404.php
@@ -15,39 +15,44 @@ get_header(); ?>
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'words' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'words' ); ?></p>
+				<div class="container">
 
-					<?php get_search_form(); ?>
+					<div class="page-content">
+						<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'words' ); ?></p>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+						<?php get_search_form(); ?>
 
-					<?php if ( words_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'words' ); ?></h2>
-						<ul>
+						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+
+						<?php if ( words_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+						<div class="widget widget_categories">
+							<h2 class="widget-title"><?php _e( 'Most Used Categories', 'words' ); ?></h2>
+							<ul>
+							<?php
+								wp_list_categories( array(
+									'orderby'    => 'count',
+									'order'      => 'DESC',
+									'show_count' => 1,
+									'title_li'   => '',
+									'number'     => 10,
+								) );
+							?>
+							</ul>
+						</div><!-- .widget -->
+						<?php endif; ?>
+
 						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+							/* translators: %1$s: smiley */
+							$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'words' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
 
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'words' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+						<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+					</div><!-- .page-content -->
 
-				</div><!-- .page-content -->
+				</div>
+
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
