7285cec5cfbf36f2b8890de276d9afaba046461e
Matt Dunn <matt@processyellow.ca>
2015-09-10 22:28:17 -0400

    Link admin link in post meta and explicitely set colour

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -1,6 +1,6 @@
 <aside class="post-meta">
     <ul>
-      <li><a href="<?php the_permalink ?>"><i class="fa fa-clock-o"></i><?php the_date(); ?></a></li>
-      <li><i class="fa fa-user"></i><?php the_author(); ?></li>
+      <li><a href="<?php the_permalink() ?>"><i class="fa fa-clock-o"></i><?php the_date(); ?></a></li>
+      <li><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><i class="fa fa-user"></i><?php the_author(); ?></a></li>
     </ul>
 </aside>
