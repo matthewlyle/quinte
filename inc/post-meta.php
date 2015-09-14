83214dc8a200996c870e7abbcc46e65f4999f572
Matt Dunn <matt@processyellow.ca>
2015-09-14 12:01:34 -0400

    Always show date even when published on same day

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -1,6 +1,6 @@
 <aside class="post-meta">
     <ul>
-      <li><a href="<?php the_permalink() ?>"><i class="fa fa-clock-o"></i><?php the_date(); ?></a></li>
+      <li><a href="<?php the_permalink() ?>"><i class="fa fa-clock-o"></i><?php the_time( get_option( 'date_format' ) ); ?></a></li>
       <li><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><i class="fa fa-user"></i><?php the_author(); ?></a></li>
     </ul>
 </aside>
